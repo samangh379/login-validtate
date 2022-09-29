e61f43acc08178d996b27ccad4287a0bb9c92caa
Saman Gholami <samangh379@gmail.com>
2022-09-29 05:50:43 -0700

    design login page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import LoginPage from "./Components/LoginPage";
 
 const App = () => {
-    return <div></div>;
+    return (
+        <div>
+            <Routes>
+                <Route path="/" element={<LoginPage />} />
+            </Routes>
+        </div>
+    );
 };
 
 export default App;
---
+++ src/Components/LoginPage.js
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import styles from "./LoginPage.module.css";
+import ReactLogo from "../assets/download.png";
+import { useNavigate } from "react-router-dom";
+
+const LoginPage = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const navigate = useNavigate();
+
+    const sendLoginRequest = async (req) => {
+        req.preventDefault();
+        await fetch("https://dummyjson.com/auth/login", {
+            method: "POST",
+            body: JSON.stringify({
+                username: email,
+                password: password,
+            }),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8",
+            },
+        })
+            .then((response) => {
+                if (response.status === 200) {
+                    navigate("/information");
+                    alert("با موفقیت وارد شدید");
+                    return response.json();
+                } else return alert("اطلاعات وارد شده صحیح نیست");
+            })
+            .then((json) => window.localStorage.setItem("token", json.token));
+    };
+
+    return (
+        <div>
+            <div className={styles.container}>
+                <div className={styles.loginPage}>
+                    <div className={styles.leftContainer}>
+                        <div className={styles.inputHolder}>
+                            <div className={styles.registerInfo}>
+                                <label htmlFor="username">نام کاربری</label>
+                                <input
+                                    type="email"
+                                    id="username"
+                                    onChange={(e) => {
+                                        setEmail(e.target.value);
+                                    }}
+                                />
+                                <label htmlFor="password">نام کاربری</label>
+                                <input
+                                    type="password"
+                                    id="pass"
+                                    onChange={(e) => {
+                                        setPassword(e.target.value);
+                                    }}
+                                />
+                            </div>
+                        </div>
+                        <div className={styles.buttons}>
+                            <button onClick={sendLoginRequest}>ورود به حساب کاربری</button>
+                            <span>فراموشی رمز ورود</span>
+                        </div>
+                    </div>
+                    <div className={styles.rightContainer}>
+                        <img src={ReactLogo} />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default LoginPage;
